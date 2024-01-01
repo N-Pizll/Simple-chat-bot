fe5f6d1446fb0be4c71fb73389bc8c7f757a749f
Nima Khastar <nimapizll.nn@gmail.com>
2024-01-01 18:19:21 +0330

    کامل نی

---
+++ script.js
@@ -0,0 +1,29 @@
+function hello(inputHi) {
+    if (inputHi == "سلام") {
+      document.write("سلام جیگر");
+      return;
+    } else if (
+      inputHi == "hello" ||
+      inputHi == "Hello" ||
+      inputHi == "HELLO" ||
+      inputHi == "hey" ||
+      inputHi == "Hey" ||
+      inputHi == "HEY" ||
+      inputHi == "hi" ||
+      inputHi == "Hi" ||
+      inputHi == "HI"
+    ) {
+      document.write("Hello, how are you?");
+      return;
+    }
+  }
+  hello(prompt("What do you want?!"))
+  function fine(input) {
+    if(input=="خوبی"){
+      document.write("سلامتیت");
+      return;
+    }else if(
+      input==""
+    )
+  }
+  
